1d0509f6cc6622795c5f54c4f449627ad91aa385
Lip <563040043@qq.com>
2019-07-14 09:58:55 +0800

    day6: config file

---
+++ www/config_default.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Lip'
+
+# config_default.py
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'www-data',
+        'password': 'www-data',
+        'database': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Lip'
+
+# config_override.py
+
+configs = {
+    'db': {
+        'host': '127.0.0.1'
+    }
+}
