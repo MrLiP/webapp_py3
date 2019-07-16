a2af3b2164f465e480a95439c94a46f9db757b66
Lip <563040043@qq.com>
2019-07-16 11:16:10 +0800

    day8：web前端+MVC

--- www/handlers.py
+++ www/handlers.py
@@ -1,6 +1,28 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+#!/user/bin/env python3
+#-*- coding = utf-8 -*-
 
 __author__ = 'Lip'
 
+# ORM框架、Web框架和配置都已就绪，我们可以开始编写一个最简单的MVC，把它们全部启动起来
+# 通过Web框架的@get和ORM框架的Model支持，可以很容易地编写一个处理首页URL的函数：
+
 ' url handlers '
+
+import re, time, json, logging, hashlib, base64, asyncio
+
+from coroweb import get, post
+
+from models import User, Comment, Blog, next_id
+
+@get('/')
+def index(request):
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+    ]
+    return {
+        '__template__': 'blogs.html',
+        'blogs': blogs
+    }
